da4cc2cdf33bdf9a05241905bc94b74013b1fd78
Maksim <mku.dmi@gmail.com>
2025-03-29 02:14:06 +0300

    bottom nav has been improved

--- asthma-tracker/src/components/BottomNav.js
+++ asthma-tracker/src/components/BottomNav.js
@@ -1,5 +1,8 @@
 import React from 'react';
 import { NavLink } from 'react-router-dom';
+import { ReactComponent as IconMeasure } from '../assets/icons/measure.svg';
+import { ReactComponent as IconChart } from '../assets/icons/chart.svg';
+import { ReactComponent as IconUser } from '../assets/icons/user.svg';
 
 function BottomNav() {
   const navStyle = {
@@ -7,25 +10,38 @@ function BottomNav() {
     justifyContent: 'space-around',
     alignItems: 'center',
     borderTop: '1px solid #ccc',
-    padding: '10px 0'
+    padding: '10px 0',
+    backgroundColor: '#fff'
   };
 
   const linkStyle = {
     textDecoration: 'none',
-    fontSize: '14px'
+    display: 'flex',
+    flexDirection: 'column',
+    alignItems: 'center',
+    fontSize: '12px',
+    color: '#333'
+  };
+
+  const iconStyle = {
+    width: 24,
+    height: 24,
+    marginBottom: 4
   };
 
   return (
     <div style={navStyle}>
       <NavLink to="/" style={linkStyle}>
-        {/* Иконка можно вставить через <img src="..." alt="..."/> или через Material Icons */}
-        <div>Передать показания</div>
+        <IconMeasure style={iconStyle} />
+        <span>Показания</span>
       </NavLink>
       <NavLink to="/charts" style={linkStyle}>
-        <div>Графики</div>
+        <IconChart style={iconStyle} />
+        <span>Графики</span>
       </NavLink>
       <NavLink to="/user" style={linkStyle}>
-        <div>Пользователь</div>
+        <IconUser style={iconStyle} />
+        <span>Профиль</span>
       </NavLink>
     </div>
   );
