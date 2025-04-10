ff548385dec5c37bcf3e4f86d42859bcb1b21ea3
Maksim <mku.dmi@gmail.com>
2025-04-10 19:28:29 +0300

    nav update

--- src/components/BottomNav.js
+++ src/components/BottomNav.js
@@ -7,21 +7,24 @@ import { ReactComponent as IconUser } from '../assets/icons/user.svg';
 function BottomNav() {
   const navStyle = {
     display: 'flex',
-    justifyContent: 'space-around',
-    alignItems: 'center',
     borderTop: '1px solid #ccc',
-    padding: '10px 0',
-    backgroundColor: '#fff'
+    backgroundColor: '#fff',
+    height: 60
   };
 
-  const linkStyle = {
-    textDecoration: 'none',
+  const getLinkStyle = ({ isActive }) => ({
+    flex: 1,
     display: 'flex',
     flexDirection: 'column',
     alignItems: 'center',
+    justifyContent: 'center',
+    textDecoration: 'none',
     fontSize: '12px',
-    color: '#333'
-  };
+    color: isActive ? '#1976d2' : '#333',
+    backgroundColor: isActive ? '#e3f2fd' : '#fff',
+    borderRight: '1px solid #ccc',
+    height: '100%'
+  });
 
   const iconStyle = {
     width: 24,
@@ -31,20 +34,25 @@ function BottomNav() {
 
   return (
     <div style={navStyle}>
-      <NavLink to="/" style={linkStyle}>
+      <NavLink to="/" style={getLinkStyle}>
         <IconMeasure style={iconStyle} />
         <span>Показания</span>
       </NavLink>
-      <NavLink to="/charts" style={linkStyle}>
+      <NavLink to="/charts" style={getLinkStyle}>
         <IconChart style={iconStyle} />
         <span>Графики</span>
       </NavLink>
-      <NavLink to="/user" style={linkStyle}>
+      <NavLink to="/user" style={({ isActive }) => ({
+        ...getLinkStyle({ isActive }),
+        borderRight: 'none'
+      })}>
         <IconUser style={iconStyle} />
         <span>Профиль</span>
       </NavLink>
+
     </div>
   );
 }
 
 export default BottomNav;
+
